48cfa058177209292909c5b7d3b7f25c673b2f05
Parkhill4 <98786222+Parkhill4@users.noreply.github.com>
2022-01-31 22:10:43 -0600

    Add files via upload

---
+++ ScriptingA1Q1.py
@@ -0,0 +1,9 @@
+"""
+    Question 1: This program displays the students information
+"""
+name = "Ethan Parkhill"
+city = "Beeville, TX 78102"
+phone = "254-315-1012"
+major = "Cyber Security"
+
+print(name, city, phone, major)
