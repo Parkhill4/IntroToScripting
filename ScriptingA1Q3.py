76722c2f7356817fa69bb8a85f5afe2ff308b422
Parkhill4 <98786222+Parkhill4@users.noreply.github.com>
2022-01-31 22:42:42 -0600

    Add files via upload

---
+++ ScriptingA1Q3.py
@@ -0,0 +1,10 @@
+"""
+    Question 3: This program will calculate the distance of a car driving 70
+                miles per hour for different lengths of time
+"""
+
+mph = 70
+
+print("A. The distance the car travels in 6 hours is:", mph*6, "miles")
+print("B. The distance the car travels in 10 hours is:", mph*10, "miles")
+print(". The distance the car travels in 15 hours is:", mph*15, "miles")
